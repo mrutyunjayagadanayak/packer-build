28b8e489f0d7d98ff71f1196e593faace21ebda1
mrutyunjayagadanayak <mrutyunjayagadanayak@yandex.com>
2021-12-29 21:09:04 +0530

    Resource group tf file

---
+++ resourceGroup.tf
@@ -0,0 +1,26 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = ">= 2.26"
+        }
+    }
+    backend "azurerm" {
+        resource_group_name  = "cloud-shell-storage-centralindia"
+        storage_account_name = "csg1003200054b390c1"
+        container_name       = "tfstate_packer"
+        key                  = "terraform.tfstate"
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+    name        = "myResourceGroup"
+    location    = "centralindia"
+    tags        = {
+        Environment = 'Temp'
+    }
+}
