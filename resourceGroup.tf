141a85738304653ca4b62141048a9debe1cae904
mrutyunjayagadanayak <mrutyunjayagadanayak@yandex.com>
2021-12-29 21:42:40 +0530

    Update resourceGroup.tf

--- resourceGroup.tf
+++ resourceGroup.tf
@@ -5,6 +5,12 @@ terraform {
             version = ">= 2.26"
         }
     }
+    backend "azurerm" {
+        resource_group_name  = "cloud-shell-storage-centralindia"
+        storage_account_name = "csg1003200054b390c1"
+        container_name       = "tfstate_packer_RG"
+        key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
